2f29e538812e11c3094fc7f5e43267dec3e12fe1
nohkukhyun <kh.noh@qualson.com>
2020-01-16 12:33:33 +0900

    commitgit add .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react"
 import "./App.css"
 import Counter from "./components/Counter/Counter"
+import MyForm from "./components/MyForm/MyForm"
+import ReducerSample from "./ReducerSample"
 
 const App: React.FC = () => {
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form)
+  }
   return (
     <div className="App">
-      {/* <Greetings name="hello" option="typescript" onClick={handleClick} /> */}
-      <Counter />
+      <MyForm onSubmit={onSubmit} />
+      <ReducerSample />
     </div>
   )
 }
